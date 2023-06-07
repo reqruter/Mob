5ed2d223faaf3045bf8412181953aaa39f1dba1c
reqruter <reqruter@yandex.ru>
2023-06-07 21:29:57 +0300

    project changed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,7 +10,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
 
     }
 
@@ -20,6 +20,32 @@ public class CashbackHackServiceTest {
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashback1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 50;
+        int actual = service.remain(amount);
+        int expected = 950;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashback2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testRemainWithCashback3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
     }
 }
